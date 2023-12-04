ad9f16409d6b1fe1330b5bddd99c3f7eaba45a39
Neerajkumarc <chaudharyneeraj91@gmail.com>
2023-12-04 14:58:36 +0530

    📁 moved video_maker to new file

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
 import json
 import random
-from moviepy.editor import ImageClip, TextClip, CompositeVideoClip, AudioFileClip
-from breaklines import break_into_lines
+import os
+from video_maker import video_maker
 
+#path
+imagesPath = "./assets/images/"
+audioPath = "./assets/audio/"
+quotesPath = "./assets/quotes/"
 
-def video_maker(image, text, audio):
-    audio_clip = AudioFileClip(audio)
-    background_image_clip = ImageClip(image, duration=audio_clip.duration)
-    text_to_add = break_into_lines(text, 40)
-    text_clip = TextClip(text_to_add, fontsize=30, color='white', font='Helvetica-Bold')    
-    text_clip = text_clip.set_duration(background_image_clip.duration)
-    text_clip = text_clip.set_position(('center', 'center'))
-
-    final_clip = CompositeVideoClip([background_image_clip, text_clip.set_audio(audio_clip)])
-
-    final_clip.write_videofile("export.mp4", codec='libx264', audio_codec='aac', fps=1)
-
-
-#load text
-f = open("./assets/quotes/stoic.json")
+# load a random quote
+f = open(f"{quotesPath}{random.choice(os.listdir(quotesPath))}")
 data = json.load(f)
-text = (data['quotes'][random.randint(0,(len(data['quotes'])))]['quote'])
+text = (data['quotes'][random.randint(0,(len(data['quotes'])-1))]['quote'])
 
-#load audio
-audio = "./assets/audio/audio-1.mp3"
+# load a random audio
+audio = f"{audioPath}{random.choice(os.listdir(audioPath))}"
 
-#load image
-image = "./assets/images/image-1.jpg"
+# load a random image
+image = f"{imagesPath}{random.choice(os.listdir(imagesPath))}"
 
 video_maker(image, text, audio)
+
+
 
 
---
+++ video_maker.py
@@ -0,0 +1,14 @@
+from moviepy.editor import ImageClip, TextClip, CompositeVideoClip, AudioFileClip
+from breaklines import break_into_lines
+
+def video_maker(image, text, audio):
+    audio_clip = AudioFileClip(audio)
+    background_image_clip = ImageClip(image, duration=audio_clip.duration)
+    text_to_add = break_into_lines(text, 40)
+    text_clip = TextClip(text_to_add, fontsize=30, color='white', font='Helvetica-Bold')    
+    text_clip = text_clip.set_duration(background_image_clip.duration)
+    text_clip = text_clip.set_position(('center', 'center'))
+
+    final_clip = CompositeVideoClip([background_image_clip, text_clip.set_audio(audio_clip)])
+
+    final_clip.write_videofile("export.mp4", codec='libx264', audio_codec='aac', fps=1)
