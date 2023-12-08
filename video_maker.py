14c3b79ac2122c96eb75923250aa7bb50086911d
Neerajkumarc <chaudharyneeraj91@gmail.com>
2023-12-08 21:21:51 +0530

    added background to text

--- video_maker.py
+++ video_maker.py
@@ -5,7 +5,7 @@ def video_maker(image, text, audio, outputPath):
     audio_clip = AudioFileClip(audio)
     background_image_clip = ImageClip(image, duration=audio_clip.duration)
     text_to_add = break_into_lines(text, 40)
-    text_clip = TextClip(text_to_add, fontsize=30, color='white', font='Helvetica-Bold')    
+    text_clip = TextClip(text_to_add, fontsize=30, color='white', font='Helvetica-Bold', bg_color="gray1")    
     text_clip = text_clip.set_duration(background_image_clip.duration)
     text_clip = text_clip.set_position(('center', 'center'))
 
